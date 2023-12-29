ffee34db8830ac6d9041778c6da95f6ba401e639
ibadkhan1996 <ibad.khan96@gmail.com>
2023-12-29 19:47:31 +0500

    mantine dark theme corrected

--- mantine.config.js
+++ mantine.config.js
@@ -1,7 +1,11 @@
 import { Avatar, Badge, Drawer, Loader, Modal, Select } from "@mantine/core";
 
 export default {
-  colors: { themeColor: ["#e7f1ff", "#cedfff", "#9bbcff", "#6496ff", "#3876fe", "#1c62fe", "#0958ff", "#0049e4", "#0041cc", "#0037b4"] },
+  colors: {
+    // dark: ["#f3f4f6", "#e5e7eb", "#d1d5db", "#9ca3af", "#6b7280", "#4b5563", "#374151", "#1f2937", "#111827", "#030712"],
+    dark: ["#f3f4f6", "#e5e7eb", "#d1d5db", "#374151", "#374151", "#4b5563", "#1f2937", "#111827", "#111827", "#030712"],
+    themeColor: ["#e7f1ff", "#cedfff", "#9bbcff", "#6496ff", "#3876fe", "#1c62fe", "#0958ff", "#0049e4", "#0041cc", "#0037b4"],
+  },
   cursorType: "pointer",
   defaultRadius: "md",
   fontFamily: "Inter, sans-serif",
